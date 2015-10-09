7668ba83aafd5a2116e5e49d32958dcc8ea6024e
Einar <einargse@gmail.com>
2015-10-09 16:57:18 -0500

    Sprint 3 initial commit

---
+++ android/assets/scripts/basic.lua
@@ -0,0 +1,19 @@
+
+-- local Thread = luajava.bindClass("java.lang.Thread")
+function exec()
+    print("Hola Android")
+end
+
+function behavior(character)
+    --while true do
+    character:move(true)
+    character:move(true)
+    character:move(true)
+    character:move(true)
+    character:move(true)
+    character:move(true)
+    character:move(true)
+    character:move(true)
+    character:move(true)
+    --end
+end
