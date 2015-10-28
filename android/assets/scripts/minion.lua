9ae7e4f3e7dd96f7d666ca4dc86904b562126e94
Angel <angyay0@bitbucket.org>
2015-10-28 17:03:44 -0600

    IA de enemigos y generador

---
+++ android/assets/scripts/minion.lua
@@ -0,0 +1,14 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Alumno
+-- Date: 28/10/2015
+-- Time: 02:44 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+require("basic")
+
+function behavior(chrtr)
+    evaluateDir(chrtr)
+    chrtr:jump()
+end
