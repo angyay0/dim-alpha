7cc519bcce828f9952a4894c54198fb703f6a1df
DiegoArmando <diarbap.94@gmail.com>
2015-11-12 14:36:50 -0600

    sonidos y arreglos

---
+++ android/assets/space3/portal.tsx
@@ -0,0 +1,204 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="portaldrc" tilewidth="32" tileheight="32" tilecount="64">
+ <image source="portaldrc/portaldrc.png" width="256" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="part1" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="part6" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="part1" value="2"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="part6" value="2"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="part1" value="3"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="part6" value="3"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="part1" value="4"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="part6" value="4"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="part2" value="1"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="part7" value="1"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="part2" value="2"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="part7" value="2"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="part2" value="3"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="part7" value="3"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="part2" value="4"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="part7" value="4"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="part3" value="1"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="part8" value="1"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="part3" value="2"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="part8" value="2"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="part3" value="3"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="part8" value="3"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="part3" value="4"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="part8" value="4"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="part4" value="1"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="part9" value="1"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="part4" value="2"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="part9" value="2"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="part4" value="3"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="part9" value="3"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="part4" value="4"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="part9" value="4"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="part5" value="1"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="part10" value="1"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="part5" value="2"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="part10" value="2"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="part5" value="3"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="part10" value="3"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="part5" value="4"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="part10" value="4"/>
+  </properties>
+ </tile>
+</tileset>
